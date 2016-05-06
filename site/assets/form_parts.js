1dcaf97690d1f373e9e1153f0c94327769998cc6
elena.chiosa <elena.chiosa@info.uaic.ro>
2016-05-06 16:08:26 +0300

    organizare pagina formular, mici modificari la pagina de start
    veti scrie cod in fisierele din folderul form_parts

---
+++ site/assets/form_parts.js
@@ -0,0 +1,16 @@
+
+$( document ).ready(function() {
+	$("#form_title").load("form_parts/title.html");
+
+	$("#form_authors").load("form_parts/authors.html");
+
+	$("#form_description").load("form_parts/description.html");
+
+	$("#form_domain").load("form_parts/domain.html");
+
+	$("#form_stakeholders").load("form_parts/stakeholders_interests.html");
+
+	$("#form_actors").load("form_parts/actors_objectives.html");
+
+	$("#form_usecases").load("form_parts/usecases.html");
+});
