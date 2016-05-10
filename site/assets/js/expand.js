a67b1a95a8240dadfb0d5116a300ad297b3b25fa
elena.chiosa <elena.chiosa@info.uaic.ro>
2016-05-10 21:06:05 +0300

    rescriere cod expand.js... tot are bug-uri + style.css?

--- site/assets/js/expand.js
+++ site/assets/js/expand.js
@@ -1,23 +1,42 @@
 $(document).ready(function() {
 
-	$(".form_section_title").click(function() {
-		$(this).parent().children(".form_section_title").toggleClass("section_selected");
+    $(".form_section_title").click(function() {
+        $(this).toggleClass("section_selected");
 
+        var el = $(this).parent().children(".form_content");
 
-		if($(this).parent().children(".form_content").hasClass("collapse")){
-			var el = $(this).parent().children(".form_content"),
-			curHeight = el.height(),
-			autoHeight = el.css('height', 'auto').height();
-			el.height(curHeight).animate({height: autoHeight}, 500);
-		}
-		else{
-			var el = $(this).parent().children(".form_content"),
-			curHeight = el.height();
-			el.height(curHeight).animate({height: 0}, 500);
-		}
+        if ($(this).parent().children(".form_content").hasClass("collapse")) {
+            el.animate({
+                'padding-top': '15px',
+                'padding-right': '15px',
+                'padding-bottom': '15px',
+                'padding-left': '15px',
+            },100);
 
-		$(this).parent().children(".form_content").css('height', 'auto');
-		$(this).parent().children(".form_content").toggleClass("collapse");
-	});
+            el.animate({
+                height: el.scrollHeight
+            }, 10, function() {
+                $(this).height('auto');
+            });
+
+        } else {
+
+            el.animate({
+                height: el.scrollHeight
+            }, 10, function() {
+                $(this).height('0');
+            });
+
+            el.animate({
+                'padding-top': 0,
+                'padding-right': '15px',
+                'padding-bottom': 0,
+                'padding-left': '15px',
+            }, 100);
+
+        }
+
+        el.toggleClass("collapse");
+    });
 
 });
