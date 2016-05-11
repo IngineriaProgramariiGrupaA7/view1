c9cadab63ece1dc7e41a392003a6e2e32cbcbe03
Richelieu <alexandrey_t12@yahoo.com>
2016-05-12 00:30:12 +0300

    Adaugat modal pe un field. Jquery lasa de dorit
    Am reusit sa adaug acel modal pe field-ul care nu e create dinamic de
    jquery. Chiar si asa, acest modal apare dupa al 2 lea click pe Edit dupa
    ce s-a incarcat pagina..apoi se deschide la primul click pe butonul
    Edit.
    Lucrul cu jquery ma depaseste....

--- site/assets/js/form_parts_js/actors_and_objectives.js
+++ site/assets/js/form_parts_js/actors_and_objectives.js
@@ -5,6 +5,34 @@
 	    $("#form_actors").load("form_parts/actors_objectives.html", function(){
 			$("#actors_list").dynamiclist();
 	    });
-		
+	     /*$("#form_actors").load("form_parts/actors_objectives.html", function ()
+			{
+			        // Get the modal
+			    var modal = document.getElementById('myModal');
+
+			    // Get the button that opens the modal
+			    var btn = document.getElementById("myBtn");
+
+			    // Get the <span> element that closes the modal
+			    var span = document.getElementsByClassName("close")[0];
+
+			    // When the user clicks the button, open the modal 
+			    btn.onclick = function() {
+			        modal.style.display = "block";
+			    }
+
+			    // When the user clicks on <span> (x), close the modal
+			    span.onclick = function() {
+			        modal.style.display = "none";
+			    }
+
+			    // When the user clicks anywhere outside of the modal, close it
+			    window.onclick = function(event) {
+			        if (event.target == modal) {
+			            modal.style.display = "none";
+			        }
+			    }
+			});*/
     });
+
 })(jQuery);
