678b28c020b38c492e366b9031ba85edffef6ec0
elena.chiosa <elena.chiosa@info.uaic.ro>
2016-05-12 19:05:13 +0300

    merge popupul, trebuie sa adaug continut

--- site/assets/js/popup.js
+++ site/assets/js/popup.js
@@ -1,35 +1,20 @@
 $.fn.makePopup = function(modalBtn) {
 
 		var modal = $(this);
-		modal.css('display','none');
+		modal.addClass('hidden');
+		modal.removeClass('flex');
 
 		var btn = $(modalBtn);
 
-		var close = $(modal).children(".close");
+		var close = $(modal).find(".close");
 
 		btn.click(function() {
-			modal.css('display','block');
+			modal.addClass('flex');
+			modal.removeClass('hidden');
 		});
 
 		close.click(function() {
-		    modal.css('display','none');
-		});
-};
-
-$.fn.popup = function(modal) {
-
-		var modal = $(modal);
-		modal.css('display','none');
-
-		var btn = $(modalBtn);
-
-		var close = $(modal).children(".close");
-
-		btn.click(function() {
-			modal.css('display','block');
-		});
-
-		close.click(function() {
-		    modal.css('display','none');
+		    modal.addClass('hidden');
+		    modal.removeClass('flex');
 		});
 };
