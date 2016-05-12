92571cbf599c10bca96fa77e908896fcab6ebed5
Radu Solca <solca.radu@yahoo.com>
2016-05-12 12:15:28 +0300

    Apply popup.js to usecases.html (Bugged)
    It only works for the first element, not for the dynamically added elements.

--- site/assets/js/form_parts_js/usecases.js
+++ site/assets/js/form_parts_js/usecases.js
@@ -4,6 +4,7 @@
 
 	    $("#formUsecases").load("form_parts/usecases.html", function(){
 			$("#usecase_list").dynamiclist();
+			$("#usecase_list .list-item .usecase_edit_view").makePopup("#usecase_list .list-item .list-edit");
 	    });
 		
     });
