f91968ca100944b84417eb23044dfd5332b5562d
Eliza Lorena <eliza.ene@info.uaic.ro>
2016-05-16 00:22:46 +0300

    usecase dropdown actori dinamic

--- site/assets/js/form_parts_js/usecases.js
+++ site/assets/js/form_parts_js/usecases.js
@@ -3,6 +3,19 @@
     $(document).ready(function() {
 
 	    $("#formUsecases").load("form_parts/usecases.html", function(){
+			$("#usecase_list .list-edit").click(function(){
+				var $parent = $(this).closest('.list-item');
+
+				var html = '';
+				for( var key in json.actorsAndObjectives) {
+					var obj = json.actorsAndObjectives[key];
+					var name = (obj.name == '')?'Untitled':obj.name;
+					html += '<option value="'+key+'">'+name+'</option>';
+				}
+				$parent.find('.mainPopup select.actors').html(html);
+			});
+
+
 	    	$(".usecase_edit_view").makePopup("#usecase_list .list-edit");
 			$("#usecase_list").dynamiclist();
 			$(".actors_list").dynamiclist_level2();
